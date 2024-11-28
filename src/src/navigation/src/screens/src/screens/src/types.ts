47a8530f34695e5ee2dbfa349363c6604f24bb65
Lysandre Yan Oliveira de Araujo <lysandrewho.y@gmail.com>
2024-11-27 21:11:19 -0300

    Create types.ts

---
+++ src/src/navigation/src/screens/src/screens/src/types.ts
@@ -0,0 +1,11 @@
+export interface Vaga {
+  id: string;
+  quarto: string;
+  ocupacao: boolean;
+  valor: number;
+  camas: number;
+  arCondicionado: boolean;
+  refeicoes: boolean;
+  descricao: string;
+  contato: string;
+}
