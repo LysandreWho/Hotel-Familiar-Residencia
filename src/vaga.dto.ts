d5be18272c54c5dc000b4c1c48848b278fb45103
Lysandre Yan Oliveira de Araujo <lysandrewho.y@gmail.com>
2024-11-30 23:44:08 -0300

    Create vaga.dto.ts

---
+++ src/vaga.dto.ts
@@ -0,0 +1,10 @@
+export interface VagaDTO {
+  id?: number;
+  titulo: string;
+  descricao: string;
+  preco: number;
+  ocupada: boolean;
+  camas: number;
+  ventilador: boolean;
+  refeicaoInclusa: boolean;
+}
